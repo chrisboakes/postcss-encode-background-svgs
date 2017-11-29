039912bdcdaabbb3e6621fe150a185a03d87ac8e
Chris Boakes <chris@chrisboakes.com>
2017-11-29 15:11:26 +0000

    Shorter more efficient encoding

--- src/encode-svg.js
+++ src/encode-svg.js
@@ -14,15 +14,33 @@ module.exports = {
             // Ensure that the <svg /></svg> segment of the URL is not already encoded
             // If not, encode it
             if (!matchSVG.svgAlreadyEncoded) {
-                encodedURL = encodeURIComponent(matchSVG.svg);
+                encodedURL = module.exports.encodeOptimizedSVGDataUri(matchSVG.svg);
             }
             let shorthandRules = matchSVG.shorthandRules;
             // Place the encoded URL back into the value structure
-            newValue = `url('data:image/svg+xml, ${encodedURL}')${shorthandRules}`;
+            newValue = `url('data:image/svg+xml,${encodedURL}')${shorthandRules}`;
         // If our Regex doesn't match, throw an error
         } else {
             throw new Error('Background SVG syntax error - please correct your syntax and try again.');
         }
         return newValue;
+    },
+
+    /**
+     * Correctly optimise SVG
+     * Taken from: https://codepen.io/tigt/post/optimizing-svgs-in-data-uris
+     * @author Taylor Hunt
+     * @param  String svgString - SVG unencoded
+     * @return String
+     */
+    encodeOptimizedSVGDataUri(svgString) {
+        var uriPayload = encodeURIComponent(svgString) // encode URL-unsafe characters
+            .replace(/%0A/g, '') // remove newlines
+            .replace(/%20/g, ' ') // put spaces back in
+            .replace(/%3D/g, '=') // ditto equals signs
+            .replace(/%3A/g, ':') // ditto colons
+            .replace(/%2F/g, '/') // ditto slashes
+            .replace(/%22/g, '\''); // replace quotes with apostrophes (may break certain SVGs)
+        return uriPayload;
     }
 };
